d9a6c751a08a747692e01b87db84a77b90c0e4ec
Hsuan <hsuan@hsuan.app>
2022-05-11 19:09:23 +0800

    新增被誰喜歡的頁面

---
+++ src/pages/relation/beLiked.vue
@@ -0,0 +1,29 @@
+<template>
+	<view class="content">
+		我被誰喜歡了
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		text-align: center;
+		height: 400rpx;
+		margin-top: 200rpx;
+	}
+</style>
