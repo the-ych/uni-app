377fcde4b3dfc5133249bdcfe8c8bf3f4a1be8d0
hsuan1117 <34859514+hsuan1117@users.noreply.github.com>
2022-05-11 13:40:26 +0000

    Deploying to gh-pages from @ the-ych/uni-app@121fad314aeab41f7a2dc2f206f3bd577ee8816f 🚀

---
+++ precache-manifest.e697264c8efc33aa2d005e73f9307159.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d18156e70e897b75bd1e4db8fd7bdae0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b84d944a13733542c66204c119835c2",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6d3756ee0e3dd7ecb155b751a233247",
+    "url": "/static/fonts/uniicons.b6d3756e.ttf"
+  },
+  {
+    "revision": "bc25473a7133eb752cd61dca2ba23e1f",
+    "url": "/static/img/logo.bc25473a.png"
+  },
+  {
+    "revision": "3ba01b46966de6e3812e93fd87485d0d",
+    "url": "/static/img/qa.3ba01b46.png"
+  },
+  {
+    "revision": "c06e870140e87c8bef028873e8e2bf5a",
+    "url": "/static/img/release.c06e8701.png"
+  },
+  {
+    "revision": "49dd60f6ab8527896ee789a67ca77250",
+    "url": "/static/img/video.49dd60f6.png"
+  },
+  {
+    "revision": "64c1ceae2bac2b61cfca",
+    "url": "/static/js/chunk-vendors.3ac9041a.js"
+  },
+  {
+    "revision": "b9182ff92b2ede44e1da",
+    "url": "/static/js/index.129d84db.js"
+  },
+  {
+    "revision": "5f4f2b963523dd3fb69b",
+    "url": "/static/js/pages-auth-login.586bf29b.js"
+  },
+  {
+    "revision": "a2fbcfbe5e0283107701",
+    "url": "/static/js/pages-auth-login~pages-auth-register.78212d5a.js"
+  },
+  {
+    "revision": "a05c50fad7383924d9de",
+    "url": "/static/js/pages-auth-login~pages-auth-register~pages-home.cf1e1c54.js"
+  },
+  {
+    "revision": "3333d833a0e691be388e",
+    "url": "/static/js/pages-auth-register.e5e61eec.js"
+  },
+  {
+    "revision": "a7982d8e8437e6a528ea",
+    "url": "/static/js/pages-home.e2df04eb.js"
+  },
+  {
+    "revision": "dde194d4b5ada2303ca3",
+    "url": "/static/js/pages-me.c1a4809a.js"
+  },
+  {
+    "revision": "423368753ed25c0a9c34",
+    "url": "/static/js/pages-relation-beLiked.36347aa9.js"
+  },
+  {
+    "revision": "776a8172c5a7148a3972",
+    "url": "/static/js/pages-tabbar-3-detial-tabbar-3-qa-tabbar-3-qa.2a2a2711.js"
+  },
+  {
+    "revision": "dd35a5186b7c8680f003",
+    "url": "/static/js/pages-tabbar-3-detial-tabbar-3-release-tabbar-3-release.82b31640.js"
+  },
+  {
+    "revision": "cc144cce479bc07f5a7b",
+    "url": "/static/js/pages-tabbar-3-detial-tabbar-3-video-tabbar-3-video.ec165686.js"
+  },
+  {
+    "revision": "e98d265a2f63e72085ca",
+    "url": "/static/js/pages-tabbar-tabbar-3-tabbar-3.cf7a2efa.js"
+  },
+  {
+    "revision": "376440c68abe9586ab0e",
+    "url": "/static/js/pages-tabbar-tabbar-4-tabbar-4.a0234712.js"
+  }
+]);
