8bd7d76415791ca3ddd6c91ba9480d90fce15bde
hsuan <hsuan@hsuan.app>
2022-05-25 21:08:48 +0800

    新增demo版本聊天列表

---
+++ src/pages/chat.vue
@@ -0,0 +1,521 @@
+<template>
+	<!-- https://blog.csdn.net/m0_67391677/article/details/123431898 -->
+	<view class="content">
+
+		<!-- 聊天内容 -->
+		<scroll-view class="chat" scroll-y="true" scroll-with-animation="true" :scroll-into-view="scrollToView">
+			<view class="chat-main" :style="{paddingBottom:inputh+'px'}">
+				<view class="chat-ls" v-for="(item,index) in unshiftmsg" :key="index" :id="'msg'+ index">
+					<view class="chat-time" v-if="item.createTime != ''">{{changeTime(item.createTime)}}</view>
+					<view class="msg-m msg-left" v-if="item.sendName ==  friendName">
+						<image class="user-img" src=""></image>
+						<view class="message" v-if="item.TextType == 0">
+							<!-- 文字 -->
+							<view class="msg-text">{{item.sendText}}</view>
+						</view>
+						<view class="message" v-if="item.TextType == 1" @tap="previewImg(item.sendText)">
+							<!-- 图像 -->
+							<image :src="item.sendText" class="msg-img" mode="widthFix"></image>
+						</view>
+						<view class="message" v-if="item.TextType == 2" @tap="playVoice(item.sendText.voice)">
+							<!-- 音频 -->
+							<view class="msg-text voice" :style="{width:item.sendText.time*4+'rpx'}">
+								<image src="" class="voice-img"></image>
+								{{item.sendText.time}}″
+							</view>
+						</view>
+						<view class="message" v-if="item.TextType == 3" @tap="openLocation(item.sendText)">
+							<!-- 位置 -->
+							<view class="msg-map">
+								<view class="map-name">{{item.sendText.name}}</view>
+								<view class="map-address">{{item.sendText.address}}</view>
+								<!-- 如果map不起作用，就可以直接用一张图片去替代 -->
+								<map class="map" :longitude="item.sendText.longitude" :latitude="item.sendText.latitude"
+								     :markers="covers(item.sendText)"></map>
+							</view>
+						</view>
+					</view>
+					<view class="msg-m msg-right" v-if="item.sendName != friendName">
+						<image class="user-img" src=""></image>
+						<view class="message" v-if="item.TextType == 0">
+							<view class="msg-text">{{item.sendText}}</view>
+						</view>
+						<view class="message" v-if="item.TextType == 1" @tap="previewImg(item.sendText)">
+							<image :src="item.sendText" class="msg-img" mode="widthFix"></image>
+						</view>
+						<view class="message" v-if="item.TextType == 2" @tap="playVoice(item.sendText.voice)">
+							<!-- 音频 -->
+							<view class="msg-text voice" :style="{width:item.sendText.time*4+'rpx'}">
+								{{item.sendText.time}}″
+								<image src="" class="voice-img"></image>
+							</view>
+						</view>
+						<view class="message" v-if="item.TextType == 3" @tap="openLocation(item.sendText)">
+							<!-- 位置 -->
+							<view class="msg-map">
+								<view class="map-name">{{item.sendText.name}}</view>
+								<view class="map-address">{{item.sendText.address}}</view>
+								<map class="map" :longitude="item.sendText.longitude" :latitude="item.sendText.latitude"
+								     :markers="covers(item.sendText)"></map>
+							</view>
+						</view>
+					</view>
+				</view>
+			</view>
+		</scroll-view>
+		<submit @inputs="inputs" @heights="heights"></submit>
+	</view>
+</template>
+
+<script>
+import dateTime from '../common/dateTime.js';
+import submit from '../components/submit.vue';
+
+
+//音频播放
+const innerAudioContext = uni.createInnerAudioContext();
+
+export default {
+	data() {
+		return {
+			friendName: "xpq",
+			msg: [{
+				"sendName": "xpq",
+				"receviceName": "゛时光い",
+				"sendText": {
+					"address": "湖南省岳阳市湘阴县新世纪大道",
+					"latitude": 28.68925,
+					"longitude": 112.90917,
+					"name": "湘阴县政府(新世纪大道北)",
+				},
+				"createTime": "2022-01-06 12:40:12",
+				"updateTime": null,
+				"chatmState": 1,
+				"TextType": 3
+			}, {
+				"sendName": "゛时光い",
+				"receviceName": "xpq",
+				"sendText": {
+					"voice": "时光匆匆流过",
+					"time": 2 //秒
+				},
+				"createTime": "2022-01-06 12:22:12",
+				"updateTime": null,
+				"chatmState": 1,
+				"TextType": 2
+			}, {
+				"sendName": "xpq",
+				"receviceName": "゛时光い",
+				"sendText": {
+					"voice": "谢谢你",
+					"time": 60 //秒
+				},
+				"createTime": "2022-01-06 12:00:12",
+				"updateTime": null,
+				"chatmState": 1,
+				"TextType": 2
+			}, {
+				"sendName": "゛时光い",
+				"receviceName": "xpq",
+				"sendText": "这是第九条未读消息",
+				"createTime": "2022-01-03 12:22:12",
+				"updateTime": null,
+				"chatmState": 1,
+				"TextType": 0
+			},
+				{
+					"sendName": "゛时光い",
+					"receviceName": "xpq",
+					"sendText": "这是第八条未读消息",
+					"createTime": "2022-01-02 12:22:07",
+					"updateTime": null,
+					"chatmState": 1,
+					"TextType": 0
+				},
+				{
+					"sendName": "xpq",
+					"receviceName": "xpq",
+					"sendText": "这是第七条未读消息",
+					"createTime": "2021-12-19 12:22:03",
+					"updateTime": null,
+					"chatmState": 1,
+					"TextType": 0
+				},
+				{
+					"sendName": "゛时光い",
+					"receviceName": "xpq",
+					"sendText": "这是第六条未读消息",
+					"createTime": "2021-12-19 12:21:58",
+					"updateTime": null,
+					"chatmState": 1,
+					"TextType": 0
+				},
+				{
+					"sendName": "゛时光い",
+					"receviceName": "xpq",
+					"sendText": "http://demo.rageframe.com/attachment/images/2021/11/18/image_1637224530_diIlZlmm.jpeg",
+					"createTime": "2021-12-19 12:21:54",
+					"updateTime": null,
+					"chatmState": 1,
+					"TextType": 1
+				},
+				{
+					"sendName": "xpq",
+					"receviceName": "゛时光い",
+					"sendText": "http://demo2.rageframe.com/attachment/images/2021/09/01/image_1630483477_N03W37zs.jpg",
+					"createTime": "2021-12-19 12:21:48",
+					"updateTime": null,
+					"chatmState": 1,
+					"TextType": 1
+				},
+				{
+					"sendName": "゛时光い",
+					"receviceName": "xpq",
+					"sendText": "这是第三条未读消息",
+					"createTime": "2021-12-19 12:21:42",
+					"updateTime": null,
+					"chatmState": 1,
+					"TextType": 0
+				},
+				{
+					"sendName": "゛时光い",
+					"receviceName": "xpq",
+					"sendText": "这是第二条未读消息",
+					"createTime": "2021-12-19 12:21:33",
+					"updateTime": null,
+					"chatmState": 1,
+					"TextType": 0
+				},
+				{
+					"sendName": "゛时光い",
+					"receviceName": "xpq",
+					"sendText": "http://demo2.rageframe.com/attachment/images/2021/09/01/image_1630483477_N03W37zs.jpg",
+					"createTime": "2021-12-19 11:02:18",
+					"updateTime": null,
+					"chatmState": 1,
+					"TextType": 1
+				},
+				{
+					"sendName": "゛时光い",
+					"receviceName": "xpq",
+					"sendText": "爱你啊",
+					"createTime": "2021-12-18 20:37:03",
+					"updateTime": null,
+					"chatmState": 0,
+					"TextType": 0
+				}
+			],
+			// 反转数据接收
+			unshiftmsg: [],
+			imgMsg: [],
+			scrollToView: '',
+			oldTime: new Date(),
+			inputh: '60'
+		}
+	},
+	onShow() {
+		// 数组倒叙 主要是应对后端传过来的数据
+		for (var i = 0; i < this.msg.length; i++) {
+			//时间间隔处理
+			if (i < this.msg.length - 1) { //这里表示头部时间还是显示一下
+				let t = dateTime.spaceTime(this.oldTime, this.msg[i].createTime);
+				if (t) {
+					this.oldTime = t;
+				}
+				this.msg[i].createTime = t;
+			}
+			// 获取图片，为下面的预览做准备
+			if (this.msg[i].TextType == 1) {
+				this.imgMsg.unshift(this.msg[i].sendText)
+			}
+			this.unshiftmsg.unshift(this.msg[i]);
+		}
+		// 跳转到最后一条数据 与前面的:id进行对照
+		this.$nextTick(function() {
+			this.scrollToView = 'msg' + (this.unshiftmsg.length - 1)
+		})
+	},
+	components: {
+		submit,
+	},
+	methods: {
+		changeTime(date) {
+			return dateTime.dateTime1(date);
+		},
+		// 进行图片的预览
+		previewImg(e) {
+			let index = 0;
+			for (let i = 0; i < this.imgMsg.length; i++) {
+				if (this.imgMsg[i] == e) {
+					index = i;
+				}
+			}
+			console.log("index", index)
+			// 预览图片
+			uni.previewImage({
+				current: index,
+				urls: this.imgMsg,
+				longPressActions: {
+					itemList: ['发送给朋友', '保存图片', '收藏'],
+					success: function(data) {
+						console.log('选中了第' + (data.tapIndex + 1) + '个按钮,第' + (data.index + 1) + '张图片');
+					},
+					fail: function(err) {
+						console.log(err.errMsg);
+					}
+				}
+			});
+		},
+		//音频播放
+		playVoice(e) {
+			innerAudioContext.src = e;
+			innerAudioContext.onPlay(() => {
+				console.log('开始播放');
+			});
+		},
+		//地图定位
+		covers(e) {
+			let map = [{
+				latitude: e.latitude,
+				longitude: e.longitude,
+				//iconPath: '../../../static/logo.png'
+			}]
+			return (map);
+		},
+		//跳转地图信息
+		openLocation(e) {
+			uni.openLocation({
+				latitude: e.latitude,
+				longitude: e.longitude,
+				name: e.name,
+				address: e.address,
+				success: function() {
+					console.log('success');
+				}
+			});
+		},
+		//接受输入内容
+		inputs(e) {
+			//时间间隔处理
+			let data = {
+				"sendName": "゛时光い",
+				"receviceName": "xpq",
+				"sendText": e.message,
+				"createTime": new Date(),
+				"updateTime": new Date(),
+				"chatmState": 1,
+				"TextType": e.type
+			};
+			// 发送给服务器消息
+			// onSendWS(JSON.stringify(data));
+
+			this.unshiftmsg.push(data);
+			// 跳转到最后一条数据 与前面的:id进行对照
+			this.$nextTick(function() {
+				this.scrollToView = 'msg' + (this.unshiftmsg.length - 1)
+			})
+			if (e.type == 1) {
+				this.imgMsg.push(e.message);
+			}
+			console.log(e)
+		},
+		//输入框高度
+		heights(e) {
+			console.log("高度:", e)
+			this.inputh = e;
+			this.goBottom();
+		},
+		// 滚动到底部
+		goBottom() {
+			this.scrollToView = '';
+			this.$nextTick(function() {
+				this.scrollToView = 'msg' + (this.unshiftmsg.length - 1)
+			})
+		}
+	}
+}
+</script>
+
+<style lang="scss">
+page {
+	height: 100%;
+}
+
+
+.place-bottom {
+	position: fixed;
+	bottom: var(--window-bottom);
+	left: 0;
+	right: 0;
+	height: calc(0.5 * 125rpx);
+	line-height: calc(0.5 *100rpx);
+	background: #fff;
+}
+
+.content {
+	height: 100%;
+	background-color: rgba(244, 244, 244, 1);
+}
+
+.chat {
+	height: 100%;
+
+	.chat-main {
+		padding-left: 32rpx;
+		padding-right: 32rpx;
+		padding-top: 20rpx;
+		// padding-bottom: 120rpx;  //获取动态高度
+		display: flex;
+		flex-direction: column;
+	}
+
+	.chat-ls {
+		.chat-time {
+			font-size: 24rpx;
+			color: rgba(39, 40, 50, 0.3);
+			line-height: 34rpx;
+			padding: 10rpx 0rpx;
+			text-align: center;
+		}
+
+		.msg-m {
+			display: flex;
+			padding: 20rpx 0;
+
+			.user-img {
+				flex: none;
+				width: 80rpx;
+				height: 80rpx;
+				border-radius: 20rpx;
+			}
+
+			.message {
+				flex: none;
+				max-width: 480rpx;
+			}
+
+			.msg-text {
+				font-size: 32rpx;
+				color: rgba(39, 40, 50, 1);
+				line-height: 44rpx;
+				padding: 18rpx 24rpx;
+			}
+
+			.msg-img {
+				max-width: 400rpx;
+				border-radius: 20rpx;
+			}
+
+			.msg-map {
+				background: #fff;
+				width: 464rpx;
+				height: 284rpx;
+				overflow: hidden;
+
+				.map-name {
+					font-size: 32rpx;
+					color: rgba(39, 40, 50, 1);
+					line-height: 44rpx;
+					padding: 18rpx 24rpx 0 24rpx;
+					//下面四行是单行文字的样式
+					display: -webkit-box;
+					-webkit-box-orient: vertical;
+					-webkit-line-clamp: 1;
+					overflow: hidden;
+				}
+
+				.map-address {
+					font-size: 24rpx;
+					color: rgba(39, 40, 50, 0.4);
+					padding: 0 24rpx;
+					//下面四行是单行文字的样式
+					display: -webkit-box;
+					-webkit-box-orient: vertical;
+					-webkit-line-clamp: 1;
+					overflow: hidden;
+				}
+
+				.map {
+					padding-top: 8rpx;
+					width: 464rpx;
+					height: 190rpx;
+				}
+			}
+
+			.voice {
+				// width: 200rpx;
+				min-width: 100rpx;
+				max-width: 400rpx;
+			}
+
+			.voice-img {
+				width: 28rpx;
+				height: 36rpx;
+			}
+		}
+
+		.msg-left {
+			flex-direction: row;
+
+			.msg-text {
+				margin-left: 16rpx;
+				background-color: #fff;
+				border-radius: 0rpx 20rpx 20rpx 20rpx;
+			}
+
+			.ms-img {
+				margin-left: 16rpx;
+			}
+
+			.msh-map {
+				margin-left: 16rpx;
+				border-radius: 0rpx 20rpx 20rpx 20rpx;
+			}
+
+			.voice {
+				text-align: right;
+
+			}
+
+			.voice-img {
+				float: left;
+				transform: rotate(180deg);
+				width: 28rpx;
+				height: 36rpx;
+				padding-bottom: 4rpx;
+			}
+		}
+
+		.msg-right {
+			flex-direction: row-reverse;
+
+			.msg-text {
+				margin-right: 16rpx;
+				background-color: rgba(255, 228, 49, 0.8);
+				border-radius: 20rpx 0rpx 20rpx 20rpx;
+			}
+
+			.ms-img {
+				margin-right: 16rpx;
+			}
+
+			.msh-map {
+				margin-left: 16rpx;
+				border-radius: 20rpx 0rpx 20rpx 20rpx;
+			}
+
+			.voice {
+				text-align: left;
+
+			}
+
+			.voice-img {
+				float: right;
+				padding: 4rpx;
+				width: 28rpx;
+				height: 36rpx;
+			}
+		}
+	}
+}
+</style>
---
+++ src/pages/chatList.vue
@@ -0,0 +1,48 @@
+<template>
+	<uni-list>
+		<uni-list-item
+			v-for="chat in chats" :key="chat.id"
+			:title="chat.name"
+			:note="chat.lastMessage"
+			showArrow
+			thumb="https://vkceyugu.cdn.bspapp.com/VKCEYUGU-dc-site/460d46d0-4fcc-11eb-8ff1-d5dcf8779628.png"
+			thumb-size="lg"
+			:rightText="chat.updated_at"
+		/>
+	</uni-list>
+</template>
+
+<script>
+import UniList from "../uni_modules/uni-list/uni-list";
+import UniListItem from "../uni_modules/uni-list-item/uni-list-item";
+
+export default {
+	name: "chatList",
+	data() {
+		return {
+			chats: [
+				{
+					chat_type: 'user',
+					noreadnum: '',
+					data: '',
+					name: 'test',
+					id: '566',
+					avatar: '',
+					lastMessage:'笨蛋： 我是笨蛋',
+					updated_at: '十分鐘前'
+				}
+			],
+			options: []
+		}
+	},
+	components: {UniList, UniListItem}
+}
+</script>
+
+<style scoped>
+
+.chat-custom-text {
+	font-size: 24rpx;
+	color: #999;
+}
+</style>
