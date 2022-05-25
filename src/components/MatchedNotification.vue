4d9cd818e43b52a440408622a1cf1005c130cbb3
hsuan <hsuan@hsuan.app>
2022-05-25 18:55:55 +0800

    新增通知時間顯示

--- src/components/MatchedNotification.vue
+++ src/components/MatchedNotification.vue
@@ -3,8 +3,8 @@
 		<view class="read">
 			<text v-if="this.notification.read_at === null">●</text>
 		</view>
-		<view class="icon">
-
+		<view class="icon-container">
+			<image :src="this.notification.data.icon" class="icon"></image>
 		</view>
 		<view class="title_message_container">
 			<view class="title">
@@ -13,18 +13,27 @@
 			<view class="message">
 				{{ this.notification.data.message }}
 			</view>
+			<view class="time">
+				{{ time }}
+			</view>
 		</view>
 	</view>
 </template>
 
 <script>
-import request from "../common/request";
+import moment from 'moment';
+
+import 'moment/locale/zh-tw';
+
+moment.locale('zh-tw');
 
 export default {
 	name: "MatchedNotification",
 	props: ['notification'],
 	data() {
-		return {}
+		return {
+			time: moment(this.notification.created_at).fromNow()
+		}
 	},
 	methods: {}
 }
@@ -33,10 +42,12 @@ export default {
 <style scoped>
 .container {
 	color: black;
-	margin: 30rpx;
+	margin: 10rpx 30rpx;
 	text-align: left;
 	display: flex;
 	flex-direction: row;
+	align-items: center;
+	justify-items: center;
 }
 
 .title_message_container {
@@ -49,14 +60,26 @@ export default {
 	font-weight: bold;
 }
 
+.icon-container {
+	height: 100rpx;
+	width: 50rpx;
+	margin: 0 10rpx;
+	display: flex;
+	align-items: center;
+}
+
 .icon {
-	height: 80rpx;
-	width: 80rpx;
+	height: 50rpx;
+	width: 50rpx;
 }
 
 .message {
 	font-size: 12pt;
+}
 
+.time {
+	font-size: 10pt;
+	color: grey;
 }
 
 .read {
